38b97c5208fef3b033be6f2be2c40fc714301029
UrbanWill <willian.urban@gmail.com>
2023-05-09 15:42:13 +0700

    WIP basic responsive navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 
+// Components
+import Navbar from "@/components/Navbar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,7 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <>
+          <Navbar />
+          {children}
+        </>
+      </body>
     </html>
   );
 }
