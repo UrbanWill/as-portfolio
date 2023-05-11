50ed2b05cd9bfd2e4872c89e2d841581842e96c9
UrbanWill <willian.urban@gmail.com>
2023-05-11 16:28:25 +0700

    added react-query dev tools

--- src/utils/Provider.tsx
+++ src/utils/Provider.tsx
@@ -2,13 +2,19 @@
 
 import React from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function Providers({ children }: React.PropsWithChildren) {
   const [client] = React.useState(
     new QueryClient({ defaultOptions: { queries: { staleTime: 5000 } } })
   );
 
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default Providers;
