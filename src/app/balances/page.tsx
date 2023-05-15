f9329690382658534beb688297b9df5cc7cee42c
UrbanWill <willian.urban@gmail.com>
2023-05-15 15:04:49 +0700

    implemented Token table into Balances and Indexes page

--- src/app/balances/page.tsx
+++ src/app/balances/page.tsx
@@ -4,28 +4,23 @@
 import useGetTokenBalances from "@/hooks/useGetTokenBalances";
 import { hooks } from "@/connectors/metamask";
 
+// Components
+import TokenTable from "@/components/TokenTable";
+
 export default function Balances() {
   const { isLoading, data } = useGetTokenBalances({ isNFTIndex: false });
   const { useIsActive } = hooks;
 
   const isActive = useIsActive();
 
-  const tokenBalances = data?.tokenBalances.map((token) => {
-    return (
-      <div key={token.tokenName} className="flex space-x-3">
-        <h2>Name: {token.tokenName}</h2>
-        <p>Balance: {token.tokenBalance}</p>
-      </div>
-    );
-  });
-
-  const getBody = () => {
-    return isLoading ? <p>Loading...</p> : tokenBalances;
-  };
   return (
     <main className="flex justify-center flex-col">
       <h1 className="font-bold text-2xl">Tokens Balances</h1>
-      {isActive ? getBody() : <p>Connect to Metamask</p>}
+      {isActive ? (
+        <TokenTable isLoading={isLoading} data={data?.tokenBalances ?? []} />
+      ) : (
+        <p>Connect to Metamask</p>
+      )}
     </main>
   );
 }
--- src/app/nftsIndexes/page.tsx
+++ src/app/nftsIndexes/page.tsx
@@ -4,29 +4,23 @@
 import useGetTokenBalances from "@/hooks/useGetTokenBalances";
 import { hooks } from "@/connectors/metamask";
 
+// Components
+import TokenTable from "@/components/TokenTable";
+
 export default function TokenIndexes() {
   const { isLoading, data } = useGetTokenBalances({ isNFTIndex: true });
   const { useIsActive } = hooks;
 
   const isActive = useIsActive();
 
-  const NFTIndexes = data?.tokenBalances.map((token) => {
-    return (
-      <div key={token.tokenName} className="flex space-x-3">
-        <h2>Name: {token.tokenName}</h2>
-        <p>Balance: {token.tokenBalance}</p>
-      </div>
-    );
-  });
-
-  const getBody = () => {
-    return isLoading ? <p>Loading...</p> : NFTIndexes;
-  };
-
   return (
     <main className="flex justify-center flex-col">
       <h1 className="font-bold text-2xl">Nfts Indexes Balances</h1>
-      {isActive ? getBody() : <p>Connect to Metamask</p>}
+      {isActive ? (
+        <TokenTable isLoading={isLoading} data={data?.tokenBalances ?? []} />
+      ) : (
+        <p>Connect to Metamask</p>
+      )}
     </main>
   );
 }
